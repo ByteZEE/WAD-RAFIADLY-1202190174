7cb68968d620ba25342a4884fdb9f65cfb75572d
ByteZEE <muhammadrafiadly@gmail.com>
2021-11-21 23:37:40 +0700

    Init commit

--- MODUL3 RAFIADLY/database/modul3.sql
+++ MODUL3 RAFIADLY/database/modul3.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 21, 2021 at 05:25 PM
+-- Generation Time: Nov 21, 2021 at 05:35 PM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.11
 
