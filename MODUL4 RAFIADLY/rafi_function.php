1fb5d3e288e6d93ef2627b163c0c09ed2186a90a
Muhammad Raihan Januar <71952241+KRiPTO9TE@users.noreply.github.com>
2021-11-29 10:50:02 +0700

    debugging

--- MODUL4 RAFIADLY/rafi_function.php
+++ MODUL4 RAFIADLY/rafi_function.php
@@ -1,59 +1,59 @@
-<?php
-    // koneksi ke database
-    $conn = mysqli_connect("localhost", "root", "", "wad_modul4");
+                        <?php
+                            // koneksi ke database
+                            $conn = mysqli_connect("loca1host", "root", "", "wad_modul4");
 
-    // query
-    function query($query) {
-        global $conn;
-        $result = mysqli_query($conn, $query);
-        $rows = [];
-        while ( $row = mysqli_fetch_assoc($result)) {
-            $rows[] = $row;
-        }
-        return $rows;
-    }
+                            // query
+                            function query($query) {
+                                global $conn;
+                                $result = mysqli_query($conn, $query);
+                                $rows = [];
+                                while ( $row = mysqli_fetch_assoc($result)) {
+                                    $rows[] = $row;
+                                }
+                                return $rows;
+                            }
 
-    // registrasi
-    function registrasi($data) {
-        global $conn;
+                            // registrasi
+                            function registrasi($data) {
+                                global $conn;
 
-        $nama = ($data["nama"]);
-        $email = ($data["email"]);
-        $noHP = ($data["noHP"]);
-        $sandi = mysqli_real_escape_string($conn, $data["sandi"]);
-        $confirmSandi = mysqli_real_escape_string($conn, $data["confirmSandi"]);
+                                $nama = ($data["nama"]);
+                                $email = ($data["email"]);
+                                $noHP = ($data["noHP"]);
+                                $sandi = mysqli_real_escape_string($conn, $data["sandi"]);
+                                $confirmSandi = mysqli_real_escape_string($conn, $data["confirmSandi"]);
 
-        // konfirmasi sandi
-        if ($sandi != $confirmSandi) {
-            echo '<script language="javascript">';
-            echo 'alert("Konfirmasi sandi salah!")';
-            echo '</script>';
-            return false;
-        }
+                                // konfirmasi sandi
+                                if ($sandi != $confirmSandi) {
+                                    echo '<script language="javascript">';
+                                    echo 'alert("Konfirmasi sandi salah!")';
+                                    echo '</script>';
+                                    return false;
+                                }
 
-        // cek apakah email sudah terdaftar atau belum
-        $result = mysqli_query($conn, "SELECT email FROM users WHERE email = '$email'");
+                                // cek apakah email sudah terdaftar atau belum
+                                $result = mysqli_query($conn, "SELECT email FROM users WHERE email = '$email'");
 
-        if( mysqli_fetch_assoc($result)) {
-            echo '<script language="javascript">';
-            echo 'alert("Email sudah terdaftar!")';
-            echo '</script>';
-            return false;
-        }
+                                if( mysqli_fetch_assoc($result)) {
+                                    echo '<script language="javascript">';
+                                    echo 'alert("Email sudah terdaftar!")';
+                                    echo '</script>';
+                                    return false;
+                                }
 
-        // enkripsi password
-        $sandi = password_hash($sandi, PASSWORD_DEFAULT);
+                                // enkripsi password
+                                $sandi = password_hash($sandi, PASSWORD_DEFAULT);
 
-        // menambahkan data ke database
-        mysqli_query($conn, "INSERT INTO users VALUES ('', '$nama', '$email', '$sandi', '$noHP')");
+                                // menambahkan data ke database
+                                mysqli_query($conn, "INSERT INTO users VALUES ('', '$nama', '$email', '$sandi', '$noHP')");
 
-        return mysqli_affected_rows($conn);
-    }
-    
-    function hapus($id) {
-        global $conn;
-        mysqli_query($conn, "DELETE FROM bookings WHERE id = $id");
-        return mysqli_affected_rows($conn);
-    }
-    
-?>
+                                return mysqli_affected_rows($conn);
+                            }
+
+                            function hapus($id) {
+                                global $conn;
+                                mysqli_query($conn, "DELETE FROM bookings WHERE id = $id");
+                                return mysqli_affected_rows($conn);
+                            }
+
+                        ?>
